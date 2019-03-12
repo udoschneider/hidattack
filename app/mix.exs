e33f7f44e4f299b77ca7705c7a70833b7d652c38
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-03-12 09:47:10 +0100

    Added hidraw

--- app/mix.exs
+++ app/mix.exs
@@ -24,6 +24,8 @@ defmodule App.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      {:hidraw, git: "https://github.com/krodelin/hidraw.git", branch: "develop"},
     ]
   end
 end
