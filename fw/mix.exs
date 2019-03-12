968eef144b9de81957993ad44ee6b7fa16dea81e
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-03-12 09:22:51 +0100

    Added custom system

--- fw/mix.exs
+++ fw/mix.exs
@@ -1,7 +1,7 @@
 defmodule Fw.MixProject do
   use Mix.Project
 
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64, :hidattack_bbb]
 
   def project do
     [
@@ -52,6 +52,7 @@ defmodule Fw.MixProject do
       {:nerves_system_rpi3a, "~> 1.6", runtime: false, targets: :rpi3a},
       {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
       {:nerves_system_x86_64, "~> 1.6", runtime: false, targets: :x86_64},
+      {:hidattack_system_bbb, path: "../../hidattack_system_bbb", runtime: false, targets: :hidattack_bbb},
     ]
   end
 end
