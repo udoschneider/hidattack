0d807093aaf0b6c24fb79f16dd1c248616c7f1cb
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-04-01 20:47:11 +0200

    UI Working with debug output

--- ui/lib/ui_web/controllers/page_controller.ex
+++ ui/lib/ui_web/controllers/page_controller.ex
@@ -1,11 +1,20 @@
 defmodule UiWeb.PageController do
   use UiWeb, :controller
 
+  require Logger
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def attack(conn, _params) do
+  def attack(conn, params) do
+    case Map.get(params, "level", nil) do
+      "0" -> Logger.debug(fn -> "Clear Attacks" end)
+      "1" -> Logger.debug(fn -> "Ransom" end)
+      "2" -> Logger.debug(fn -> "Wiggle" end)
+      "3" -> Logger.debug(fn -> "Mirror" end)
+      _ -> nil
+    end
     render(conn, "attack.html", layout: {UiWeb.LayoutView, "attack.html"})
   end
 end
