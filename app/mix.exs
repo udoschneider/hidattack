ad30ea643120796d1ff9b2ad8be4965b6155a93b
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-04-01 21:57:28 +0200

    Attacks working

--- app/mix.exs
+++ app/mix.exs
@@ -30,6 +30,7 @@ defmodule App.MixProject do
         tag: "v0.2.1-u1"
       },
       {:usb_gadget, git: "https://github.com/nerves-project/usb_gadget.git"},
+      {:sshex, "2.2.1"},
     ]
   end
 end
