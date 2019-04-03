7cfa609b0449bd2c936ccc8e95d6b8869faffd9f
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-04-03 08:57:25 +0200

    Extracted Demo Code, reduced wiggle amplitude

--- ui/lib/ui_web/controllers/page_controller.ex
+++ ui/lib/ui_web/controllers/page_controller.ex
@@ -9,26 +9,12 @@ defmodule UiWeb.PageController do
 
   def attack(conn, params) do
     case Map.get(params, "action", nil) do
-      "clear" -> Logger.debug(fn -> "Clear Attacks" end)
-                 App.AttackProxy.wiggle(1, 0)
-                 App.AttackProxy.mirror(0, 1)
-                 App.Ivi.clear_attack()
-      "ransom" -> Logger.debug(fn -> "Ransom" end)
-                  App.AttackProxy.wiggle(1, 0)
-                  App.AttackProxy.mirror(0, 1)
-                  App.Ivi.start_attack(1)
-      "wiggle" -> Logger.debug(fn -> "Wiggle" end)
-                  App.AttackProxy.wiggle(1, 0.5)
-                  App.AttackProxy.mirror(0, 1)
-                  App.Ivi.start_attack(2)
-      "mirror" -> Logger.debug(fn -> "Mirror" end)
-                  App.AttackProxy.wiggle(1, 0)
-                  App.AttackProxy.mirror(0, -1)
-                  App.Ivi.start_attack(3)
-      "reboot_ivi" -> Logger.debug(fn -> "Reboot IVI" end)
-                      App.Ivi.reboot()
-      "reboot_hid" -> Logger.debug(fn -> "Reboot HID" end)
-                      Nerves.Runtime.reboot()
+      "clear" -> App.AttackDemo.clear()
+      "ransom" -> App.AttackDemo.ransom_note()
+      "wiggle" -> App.AttackDemo.wiggle()
+      "mirror" -> App.AttackDemo.mirror
+      "reboot_ivi" -> App.AttackDemo.reboot_ivi()
+      "reboot_hid" -> App.AttackDemo.reboot_hid()
       _ -> nil
     end
     render(conn, "attack.html", layout: {UiWeb.LayoutView, "attack.html"})
