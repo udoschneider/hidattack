f45bdcb8146088fef9bd13d2d56aac7d4137ef01
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-07-15 13:44:47 +0200

    Removed IVI calls and functionality

--- ui/lib/ui_web/controllers/page_controller.ex
+++ ui/lib/ui_web/controllers/page_controller.ex
@@ -10,10 +10,8 @@ defmodule UiWeb.PageController do
   def attack(conn, params) do
     case Map.get(params, "action", nil) do
       "clear" -> App.AttackDemo.clear()
-      "ransom" -> App.AttackDemo.ransom_note()
       "wiggle" -> App.AttackDemo.wiggle()
       "mirror" -> App.AttackDemo.mirror
-      "reboot_ivi" -> App.AttackDemo.reboot_ivi()
       "reboot_hid" -> App.AttackDemo.reboot_hid()
       _ -> nil
     end
