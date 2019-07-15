8f3b1b6719193ee4c807fbfc50306ee8ed01852c
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-07-15 13:27:25 +0200

    Removed old code

--- _OLD/README.md
+++
@@ -1,2 +0,0 @@
-"Poncho" dir for HIDAttack
-
--- _OLD/host_test/test/host_test_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HostTestTest do
-  use ExUnit.Case
-  doctest HostTest
-
-  test "greets the world" do
-    assert HostTest.hello() == :world
-  end
-end
