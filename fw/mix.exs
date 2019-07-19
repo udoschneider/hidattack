efcfcb2343bf2d6388790db2abc1d964a6e06834
Udo <Udo.Schneider@homeaddress.de>
2019-07-19 21:50:49 +0200

    Update to nerves 1.5

--- fw/mix.exs
+++ fw/mix.exs
@@ -3,20 +3,33 @@ defmodule Fw.MixProject do
 
   @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64, :hidattack_bbb]
 
+  @app :fw
+
   def project do
     [
-      app: :fw,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.8",
       archives: [
-        nerves_bootstrap: "~> 1.5"
+        nerves_bootstrap: "~> 1.6"
       ],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
       aliases: [
         loadconfig: [&bootstrap/1]
       ],
-      deps: deps()
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host]
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble]
     ]
   end
 
@@ -39,8 +52,8 @@ defmodule Fw.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.4", runtime: false},
-      {:shoehorn, "~> 0.4"},
+      {:nerves, "~> 1.5", runtime: false},
+      {:shoehorn, "~> 0.6"},
       {:ring_logger, "~> 0.6"},
       {:toolshed, "~> 0.2"},
 
@@ -49,13 +62,13 @@ defmodule Fw.MixProject do
       {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.6", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.6", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.6", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.6", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.6", runtime: false, targets: :rpi3a},
-      {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.6", runtime: false, targets: :x86_64},
+      {:nerves_system_rpi, "~> 1.8", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.8", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.8", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
+      {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
+      {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64},
 
       # {:hidattack_system_bbb, path: "../../hidattack_system_bbb", runtime: false, targets: :hidattack_bbb},
       {
