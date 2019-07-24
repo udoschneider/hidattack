f851a8151f3a9e2b1fd28386ca53b4828ac931a0
Udo <Udo.Schneider@homeaddress.de>
2019-07-25 01:37:01 +0200

    Refactoring and constant renaming

--- app/lib/app/attack_demo.ex
+++ app/lib/app/attack_demo.ex
@@ -3,33 +3,30 @@ defmodule App.AttackDemo do
   require Logger
 
   @wiggle_normal_frequency 2.0
-  @wiggle_attack_frequency 2.0
-
   @wiggle_normal_amplitude 0.0
+
+  @wiggle_attack_frequency 2.0
   @wiggle_attack_amplitude 0.2
 
-  @mirror_normal_frequency 0.0
-  @mirror_attack_frequency 0.0
-
-  @mirror_normal_amplitude 1.0
-  @mirror_attack_amplitude -1.0
+  @mirror_normal 1.0
+  @mirror_attack -1.0
 
   def clear() do
     Logger.debug(fn -> "#{__MODULE__} Clear Attacks" end)
     App.AttackProxy.wiggle(@wiggle_normal_frequency, @wiggle_normal_amplitude)
-    App.AttackProxy.mirror(@mirror_normal_frequency, @mirror_normal_amplitude)
+    App.AttackProxy.mirror(@mirror_normal)
   end
 
   def wiggle do
     Logger.debug(fn -> "#{__MODULE__} Wiggle" end)
     App.AttackProxy.wiggle(@wiggle_attack_frequency, @wiggle_attack_amplitude)
-    App.AttackProxy.mirror(@mirror_normal_frequency, @mirror_normal_amplitude)
+    App.AttackProxy.mirror(@mirror_normal)
   end
 
   def mirror() do
     Logger.debug(fn -> "#{__MODULE__} Mirror" end)
     App.AttackProxy.wiggle(@wiggle_normal_frequency, @wiggle_normal_amplitude)
-    App.AttackProxy.mirror(@mirror_attack_frequency, @mirror_attack_amplitude)
+    App.AttackProxy.mirror(@mirror_attack)
   end
 
   def reboot_hid() do
