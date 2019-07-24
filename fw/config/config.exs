6527d831c831b3d9a0a15ff096c374f9f6a64e72
Udo <Udo.Schneider@homeaddress.de>
2019-07-24 17:20:32 +0200

    Added project specific SSH keys

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -30,9 +30,10 @@ config :logger, backends: [RingLogger]
 
 keys =
   [
-    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
+    # Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
+    # Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
+    # Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"]),
+    Path.join([".", "hidattack_rsa.pub"]),
   ]
   |> Enum.filter(&File.exists?/1)
 
