7354d07a8f6a2ff22f8e31cb2735207d791fea65
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-07-30 13:10:10 +0200

    Removed SSH keys from Repo

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -33,7 +33,7 @@ keys =
     # Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
     # Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
     # Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"]),
-    Path.join([".", "hidattack_rsa.pub"]),
+    Path.join([".", "../../hidattack_rsa.pub"]),
   ]
   |> Enum.filter(&File.exists?/1)
 
