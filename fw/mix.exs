6c655fdcbc33a5d2d202e90f6f38126f2ee2b425
Udo Schneider <Udo.Schneider@homeaddress.de>
2019-08-02 16:18:26 +0200

    Streamlined dependencies & version

--- fw/mix.exs
+++ fw/mix.exs
@@ -1,14 +1,14 @@
 defmodule Fw.MixProject do
   use Mix.Project
 
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
+  @all_targets [:bbb]
 
   @app :fw
 
   def project do
     [
       app: @app,
-      version: "0.1.0",
+      version: File.read!("../VERSION") |> String.trim(),
       elixir: "~> 1.8",
       archives: [
         nerves_bootstrap: "~> 1.6"
@@ -20,7 +20,10 @@ defmodule Fw.MixProject do
       ],
       deps: deps(),
       releases: [{@app, release()}],
-      preferred_cli_target: [run: :host, test: :host]
+      preferred_cli_target: [
+        run: :host,
+        test: :host
+      ]
     ]
   end
 
@@ -62,15 +65,14 @@ defmodule Fw.MixProject do
       {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
 
       # Dependencies for specific targets
-      {:nerves_system_rpi, "~> 1.8", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.8", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.8", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
+      # {:nerves_system_rpi, "~> 1.8", runtime: false, targets: :rpi},
+      # {:nerves_system_rpi0, "~> 1.8", runtime: false, targets: :rpi0},
+      # {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
+      # {:nerves_system_rpi3, "~> 1.8", runtime: false, targets: :rpi3},
+      # {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
       # {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
-      {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64},
+      # {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64},
 
-      # {:hidattack_system_bbb, path: "../../hidattack_system_bbb", runtime: false, targets: :hidattack_bbb},
       {
         :gadget_system_bbb,
         git: "https://github.com/krodelin/gadget_system_bbb.git",
